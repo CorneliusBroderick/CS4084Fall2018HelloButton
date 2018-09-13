0ffcc6e470a30efe864f893eeeff7a5bf5a77b16
CorneliusBroderick <9119124@studentmail.ul.ie>
2018-09-13 15:39:19 +0100

    Finished Hello Button app

--- app/src/main/java/e/nbroder/myapplication/MainActivity.java
+++ app/src/main/java/e/nbroder/myapplication/MainActivity.java
@@ -2,11 +2,13 @@ package e.nbroder.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int mCounter = 0;
     private TextView mMessageTextView;
 
     @Override
@@ -18,12 +20,41 @@ public class MainActivity extends AppCompatActivity {
         // Code you add goes after the setContentView
         mMessageTextView = findViewById(R.id.message_textview);
 
+        final Button resetButton = findViewById(R.id.reset_button);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCounter = 0;
+                updateView();
+            }
+        });
+
+
         // temp area
         //mMessageTextView.setText("Niall is the coolest programmer ever!!!!!");
-        Log.d("HB", "This is a log cat log. Logs are important!");
+        //Log.d("HB", "This is a log cat log. Logs are important!");
 
-
-
+        //  Example crash!!!
+        //mMessageTextView = findViewById(0);
+        //mMessageTextView.setText("This will crash!");
 
     }
+
+
+    public void handleDecrement(View view) {
+        mCounter = mCounter - 1;
+        updateView();
+    }
+
+    public void handleIncrement(View view) {
+        mCounter += 1;
+        updateView();
+    }
+
+
+    private void updateView() {
+        mMessageTextView.setText(getString(R.string.message_format, mCounter));
+    }
+
+
 }
